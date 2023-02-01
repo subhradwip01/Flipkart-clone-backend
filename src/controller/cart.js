41d5e4ac5513632ac8b86b4a7cc1ce09637e4792
subhradwip01 <subhradwip01@gmail.com>
2023-02-01 23:48:32 +0530

    Add cart items bug fix and restructuring

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -3,53 +3,59 @@ import Cart from "../models/cart.js";
 export const addCartItemsController = (req, res, next) => {
   Cart.findOne({ userId: req.user._id })
   .exec((error, cart) => {
+    if(error) {
+        return res.status(400).json({
+            message:"Something went wrong!"
+        })
+    }
     if (cart) { 
       const cartItemIndex = cart.cartItems.findIndex(
         (item) => item.productId.toString() === req.body.productId.toString()
       );
       if (cartItemIndex >= 0) {
-          console.log(typeof cart.cartItems[cartItemIndex].price)
-        cart.cartItems[cartItemIndex].quantity =
-        cart.cartItems[cartItemIndex].quantity + req.body.quantity;
-        cart.cartItems[cartItemIndex].price = cart.cartItems[cartItemIndex].price + 
-        req.body.price * req.body.quantity;
-        cart.save((error, cart) => {
-          if (error) {
-            return res.status(400).json({
-              error,
-            });
-          }
-          if (cart) {
+        Cart.findOneAndUpdate({userId:req.user._id,"cartItems.productId":req.body.productId},{
+            "$set":{
+                "cartItems.$":{
+                    ...req.body,
+                    quantity:req.body.quantity+cart.cartItems[cartItemIndex].quantity,
+                }
+            }
+        })
+        .exec((error,newCart)=>{
+            if(error) {
+                return res.status(400).json({error})
+            }
             return res.status(200).json({
-              cart,
-            });
-          }
-        });
+                newCart
+            })
+        })
       } else {
-        cart.cartItems.push(req.body)
-        cart.save((error, cart) => {
-          if (error) {
-            return res.status(400).json({
-              error,
-            });
-          }
-          if (cart) {
+        Cart.findOneAndUpdate({userId:req.user._id},{
+            "$push":{
+                "cartItems":{
+                    ...req.body,
+                }
+            }
+        })
+        .exec((error,newCart)=>{
+            if(error) {
+                return res.status(400).json({error})
+            }
             return res.status(200).json({
-              cart,
-            });
-          }
-        });
+                newCart
+            })
+        })
       }
+      
     } else {
-      const cartItem = {
-        productId:req.body.productId,
-        price:req.body.price,
-        quantity:req.body.quantity
-        
-      }
       const cart = new Cart({
         userId: req.user._id,
-        cartItems: [cartItem],
+        cartItems: [{
+            productId:req.body.productId,
+            price:req.body.price,
+            quantity:req.body.quantity
+            
+          }],
       });
       cart.save((error, cart) => {
         if (error) {
