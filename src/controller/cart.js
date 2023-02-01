1dfdb28f184128fed2c3ca774a2b284147b4bd96
subhradwip01 <subhradwip01@gmail.com>
2023-02-01 23:57:31 +0530

    Code enhancement in cart

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -12,40 +12,35 @@ export const addCartItemsController = (req, res, next) => {
       const cartItemIndex = cart.cartItems.findIndex(
         (item) => item.productId.toString() === req.body.productId.toString()
       );
+      let condition, action;
       if (cartItemIndex >= 0) {
-        Cart.findOneAndUpdate({userId:req.user._id,"cartItems.productId":req.body.productId},{
+        condition = {userId:req.user._id,"cartItems.productId":req.body.productId}
+        action = {
             "$set":{
                 "cartItems.$":{
                     ...req.body,
                     quantity:req.body.quantity+cart.cartItems[cartItemIndex].quantity,
                 }
             }
-        })
-        .exec((error,newCart)=>{
-            if(error) {
-                return res.status(400).json({error})
-            }
-            return res.status(200).json({
-                newCart
-            })
-        })
+        }
       } else {
-        Cart.findOneAndUpdate({userId:req.user._id},{
+        condition = {userId:req.user._id};
+        action = {
             "$push":{
                 "cartItems":{
                     ...req.body,
                 }
             }
-        })
-        .exec((error,newCart)=>{
-            if(error) {
-                return res.status(400).json({error})
-            }
-            return res.status(200).json({
-                newCart
-            })
-        })
+        }
       }
+      Cart.findOneAndUpdate(condition,action,{new: true}).exec((error,newCart)=>{
+        if(error) {
+            return res.status(400).json({error})
+        }
+        return res.status(200).json({
+            newCart
+        })
+    })
       
     } else {
       const cart = new Cart({
